d52bd399c000fd5f0e1d5b7898410fa6752b62f4
Sidney Marcelo Franco <sidneymarcelofranco@hotmail.com>
2025-08-29 16:20:15 -0300

    refatorar: adicionar rota inicial para redirecionamento no Flask

--- api/index.py
+++ api/index.py
@@ -1,18 +1,11 @@
 from steex import create_app
+from flask import redirect, url_for
 
 app = create_app()
 
+@app.route('/')
+def home():
+    return redirect(url_for('dashboards.index'))
+
 if __name__ == '__main__':
     app.run()
-
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def home():
-#     return 'Hello, World!'
-
-# @app.route('/about')
-# def about():
-#     return 'About'
